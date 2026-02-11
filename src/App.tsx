c13bee9901e547c3ee1d386fe4a2299306cbbac5
paulov <paulos_50@hotmail.com>
2026-02-11 11:31:31 -0500

    refactor: se re-organiza el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { LpNavbar1 } from "@/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1";
-import { HeroSection2 } from "@/components/pro-blocks/landing-page/hero-sections/hero-section-2";
-import { LogoSection10 } from "@/components/pro-blocks/landing-page/logo-sections/logo-section-7";
-import TestimonialsSection1 from "@/components/pro-blocks/landing-page/testimonials-sections/testimonials-section-1";
-import { BentoGrid6 } from "@/components/pro-blocks/landing-page/bento-grids/bento-grid-6";
-import { FeatureSection9 } from "@/components/pro-blocks/landing-page/feature-sections/feature-section-9";
-import { StatsSection4 } from "@/components/pro-blocks/landing-page/stats-sections/stats-section-4";
-import { PricingSection3 } from "@/components/pro-blocks/landing-page/pricing-sections/pricing-section-3";
-import { FaqSection2 } from "@/components/pro-blocks/landing-page/faq-sections/faq-section-2";
-import { Footer1 } from "@/components/pro-blocks/landing-page/footers/footer-1";
+import { LpNavbar1 } from '@/sections/shared/components/pro-blocks/landing-page/lp-navbars/lp-navbar-1'
+import { HeroSection2 } from '@/sections/shared/components/pro-blocks/landing-page/hero-sections/hero-section-2'
+import { LogoSection10 } from '@/sections/shared/components/pro-blocks/landing-page/logo-sections/logo-section-7'
+import TestimonialsSection1 from '@/sections/shared/components/pro-blocks/landing-page/testimonials-sections/testimonials-section-1'
+import { BentoGrid6 } from '@/sections/shared/components/pro-blocks/landing-page/bento-grids/bento-grid-6'
+import { FeatureSection9 } from '@/sections/shared/components/pro-blocks/landing-page/feature-sections/feature-section-9'
+import { StatsSection4 } from '@/sections/shared/components/pro-blocks/landing-page/stats-sections/stats-section-4'
+import { PricingSection3 } from '@/sections/shared/components/pro-blocks/landing-page/pricing-sections/pricing-section-3'
+import { FaqSection2 } from '@/sections/shared/components/pro-blocks/landing-page/faq-sections/faq-section-2'
+import { Footer1 } from '@/sections/shared/components/pro-blocks/landing-page/footers/footer-1'
 
 function App() {
   return (
@@ -34,7 +34,7 @@ function App() {
       <FaqSection2 />
       <Footer1 />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
